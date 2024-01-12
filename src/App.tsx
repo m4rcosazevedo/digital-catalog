0775244b071b2461702724bb97f1b840eda0c7ed
Marcos Azevedo <m4rcos.azevedo@gmail.com>
2024-01-11 23:52:23 -0300

    added shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from './components/ui/button'
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1 className="text-3xl">Hello world!</h1>
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
