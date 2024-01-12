ae8e8cc79dd1d2399e1f1fb3c77e1dd3f549c087
Marcos Azevedo <marcos@MacBook-Air-de-Marcos.local>
2024-01-11 22:56:58 -0300

    initial config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
+    tailwindcss: {},
+    autoprefixer: {}
+  },
+  theme: {
+    extend: {}
+  }
+};
